ef550b3e513bdf8905f8ef1f82eeb87ac45f7895
MelFxrd <qdaewsfrftgf123@gmail.com>
2025-10-08 15:59:40 +0300

    Добавил тестовую CRUD и логику

--- Warehouse Management System/MainWindow.xaml.cs
+++ Warehouse Management System/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Warehouse_Management_System.Data;
+using Warehouse_Management_System.Models;
+using Warehouse_Management_System.ViewModels;
 
 namespace Warehouse_Management_System
 {
@@ -19,6 +22,28 @@ namespace Warehouse_Management_System
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ProductViewModel();
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            using var db = new WarehouseDbContext();
+            db.Products.Add(new Product { Name = "Тестовый продукт", Quantity = 10, Price = 100 });
+            db.SaveChanges();
+            if (DataContext is ProductViewModel viewModel)
+            {
+                viewModel.RefreshProducts();
+            }
+        }
+
+        private void EditProduct_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Изменить товар");
+        }
+
+        private void DeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Удалить товар");
         }
     }
 }
---
+++ Warehouse Management System/ViewModels/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse_Management_System.Data;
+using Warehouse_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Warehouse_Management_System.ViewModels
+{
+    public class ProductViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
+
+        public ProductViewModel()
+        {
+            RefreshProducts();
+        }
+
+        public void RefreshProducts()
+        {
+            Products.Clear();
+            using var db = new WarehouseDbContext();
+            var products = db.Products.ToList();
+            foreach (var p in products) Products.Add(p);
+        }
+    }
+}
