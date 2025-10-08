74c9707008b96e5f81db4233b5aa7c5a612368a6
MelFxrd <qdaewsfrftgf123@gmail.com>
2025-10-08 15:13:00 +0300

    Создал модели данных Order и Product

---
+++ Warehouse Management System/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Management_System.Models
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
